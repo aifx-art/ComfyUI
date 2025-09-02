a51a12c969ea291a007d2cdf0d423e7471e968af
AlpineVibrations <pro@macbookpro>
2025-09-01 18:14:04 -0700

    test

--- web/extensions/my-extension/my-extension.js
+++ web/extensions/my-extension/my-extension.js
@@ -1,19 +1,17 @@
-import { app } from "../../scripts/app.js";
+import { app } from "/scripts/app.js";
 
 app.registerExtension({
     name: "my-extension",
     async setup() {
-        console.log("[My Extension] setup called");
+        console.log("[My Extension] Loaded!");
 
-        // Add a test button to the toolbar
+        // Add a button to the toolbar
         const toolbar = document.querySelector("#comfy-toolbar");
         if (toolbar) {
             const btn = document.createElement("button");
             btn.innerText = "Click Me!";
             btn.onclick = () => alert("Hello from My Extension!");
             toolbar.appendChild(btn);
-        } else {
-            console.warn("[My Extension] Could not find #comfy-toolbar");
         }
     }
 });
